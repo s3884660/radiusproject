2f98337576bd139a46d8258d1bf1d2b414819b6a
Cintia Michel <s3876018@student.rmit.edu.au>
2021-10-24 16:21:07 +1100

    Add missing comma

--- radiusproject/radius/urls.py
+++ radiusproject/radius/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('', views.signin, name='signin'),
-    path('maps/', views.default_map, name='maps')
+    path('maps/', views.default_map, name='maps'),
     path('maps/', views.default_map, name='maps'),
     path('account/', include('django.contrib.auth.urls')),
 ]
