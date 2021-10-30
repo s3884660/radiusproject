3f99f77caa2ede955dfd597538485054f6c01d75
s3884660 <75563047+s3884660@users.noreply.github.com>
2021-10-30 11:35:53 +0800

    continuing work on forms

--- radiusproject/radius/forms.py
+++ radiusproject/radius/forms.py
@@ -7,3 +7,14 @@ class SignUpForm(forms.Form):
     password = forms.CharField(help_text="Enter Password", widget=forms.PasswordInput)
 
 
+class CreateActivityForm(forms.Form):
+    title = forms.CharField(help_text="Enter activity title")
+    description = forms.CharField(help_text="Enter activity description", widget=forms.Textarea)
+    # todo - address lookup
+    address = forms.CharField(help_text="Enter activity address")
+    # todo - date time validation
+    date = forms.DateField(help_text="Enter Date (dd/mm/yy)", required=False)
+    # tags - are they going to be an array of tags or a series of boolean values? Currently thinking an array,
+    # but need to look into python a little more
+    # todo - image upload
+    
