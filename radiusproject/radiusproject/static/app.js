eda51caec8157fa1bc10a7d195b5f7f14aa98e48
Jacopo <S3876518@student.rmit.edu.au>
2021-11-04 00:05:28 +1100

    map refreshes automatically
    if permission is given, map flies ot user location

--- radiusproject/radiusproject/static/app.js
+++ radiusproject/radiusproject/static/app.js
@@ -11,6 +11,14 @@ function getLocation() {
 function showPosition(position) {
   var latitude = position.coords.latitude;
   var longitude = position.coords.longitude;
+  //just for debug
   x.innerHTML = "Latitude: " + latitude + 
   "<br>Longitude: " + longitude;
+  //updates location of map to current user's location (if permission granted)
+  map.flyTo({
+    center: [longitude, latitude],
+    essential: true
+  });
 }
+
+
