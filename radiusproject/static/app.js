0cab688ce78d9a9b95b23ca0d73161c1312372fc
Cintia Michel <s3876018@student.rmit.edu.au>
2021-11-06 09:39:48 +1100

    Run load static files to update the folder

---
+++ radiusproject/static/app.js
@@ -0,0 +1,24 @@
+var latitude;
+var longitude;
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+}
+
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  //just for debug
+  x.innerHTML = "Latitude: " + latitude + 
+  "<br>Longitude: " + longitude;
+  //updates location of map to current user's location (if permission granted)
+  map.flyTo({
+    center: [longitude, latitude],
+    essential: true
+  });
+}
+
+
