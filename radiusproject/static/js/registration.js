c88f55100e9631b083f6f6dd05f90ae3cbaea246
s3884660 <75563047+s3884660@users.noreply.github.com>
2021-11-20 20:49:39 +0800

    heroku test

---
+++ radiusproject/static/js/registration.js
@@ -0,0 +1,33 @@
+var cart = document.querySelector('.counter-km');
+
+cart.addEventListener('click', function(ev) {
+
+  var tgt = ev.target;
+  if (tgt.matches('input[type="button"]')) {
+
+     var input = document.getElementById(tgt.dataset.for);
+     var currentValue = +input.value;
+     var minValue = +input.min;
+
+
+     if (tgt.value === '+') {
+        input.value = currentValue + 1;
+     }
+     else if (currentValue > minValue) {
+        input.value = currentValue - 1;
+     }
+
+  }
+});
+
+function popitup(url)
+{
+ newwindow=window.open(url,'name','height=300,width=650,screenX=400,screenY=350');
+ if (window.focus) {newwindow.focus()}
+ return false;
+}
+
+
+function avatar(option) {
+  document.getElementById("id_avatar").value = option;
+}
